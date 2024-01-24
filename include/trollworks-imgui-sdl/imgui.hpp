60f40aecd14dd2f480fca2959f80a49cfe1f184c
David Delassus <david.jose.delassus@gmail.com>
2024-01-24 23:58:05 +0100

    :heavy_plus_sign: vendor in imgui code

--- include/trollworks-imgui-sdl/imgui.hpp
+++ include/trollworks-imgui-sdl/imgui.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
 #include "../../vendor/imgui/imgui.h"
-#include "../../vendor/imgui/misc/cpp/imgui_stdlib.h"
-#include "../../vendor/imgui/backends/imgui_impl_sdl2.h"
-#include "../../vendor/imgui/backends/imgui_impl_sdlrenderer2.h"
+#include "../../vendor/imgui/imgui_stdlib.h"
+#include "../../vendor/imgui/imgui_impl_sdl2.h"
+#include "../../vendor/imgui/imgui_impl_sdlrenderer2.h"
